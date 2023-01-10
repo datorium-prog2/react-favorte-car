bc648a602188bacea2df6e869e21a16f9fc143be
Serzsh <serzants.janis@gmail.com>
2023-01-10 13:41:09 +0200

    Added star to car title

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 const carObject = {
   name: "Audi R8",
   imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/R8_Coupe_V10_performance-1.jpg/2560px-R8_Coupe_V10_performance-1.jpg",
-  description: "The Audi R8 is a mid-engine, 2-seater sports car,[2][3] which uses Audi's trademark quattro permanent all-wheel drive system.[2] It was introduced by the German car manufacturer Audi AG in 2006.",
-  inStock: false
+  description: "The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006.",
+  inStock: false,
+  isLuxus: true
 }
 
 function App() {
+
 
   return (
     <div className="car">
       <h1 className='car__title'>
         {carObject.name}
+
+        <span className='car__star'>
+          ⭐️
+        </span>
       </h1>
       <img 
         src={carObject.imageUrl}
@@ -23,12 +29,12 @@ function App() {
         {carObject.description}
       </p>
 
-      <span className='car__stock'>
+      <span className={carObject.inStock ? "car__stock" : "car__not-in-stock"}>
         {carObject.inStock ? "In stock" : "Not in stock"}
       </span>
-
     </div>
   );
 }
+
 
 export default App;
