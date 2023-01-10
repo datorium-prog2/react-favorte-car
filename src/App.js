7d5d585946ac11725c04ff2a727d1c9ebb2c2683
Serzsh <serzants.janis@gmail.com>
2023-01-10 14:09:22 +0200

    Added available colors

--- src/App.js
+++ src/App.js
@@ -5,7 +5,26 @@ const carObject = {
   imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/R8_Coupe_V10_performance-1.jpg/2560px-R8_Coupe_V10_performance-1.jpg",
   description: "The Audi R8 is a mid-engine, 2-seater sports car, which uses Audi's trademark quattro permanent all-wheel drive system. It was introduced by the German car manufacturer Audi AG in 2006.",
   inStock: false,
-  isLuxus: true
+  isLuxus: false,
+  color: [
+    {
+      name: 'Tomato Red',
+      colorCode: '#C34A2C'
+    },
+    {
+      name: 'Panther Black',
+      colorCode: '#0f0d0d'
+    },
+    {
+      name: 'Nardo Gray',
+      colorCode: '#686A6C'
+    },
+    {
+      name: 'Phthalo Green',
+      colorCode: '#0E2F19'
+    },
+
+  ]
 }
 
 function App() {
@@ -15,10 +34,11 @@ function App() {
     <div className="car">
       <h1 className='car__title'>
         {carObject.name}
-
-        <span className='car__star'>
-          ⭐️
-        </span>
+        {carObject.isLuxus && (
+          <span className='car__star'>
+            ⭐️
+          </span>
+        )}
       </h1>
       <img 
         src={carObject.imageUrl}
@@ -28,10 +48,26 @@ function App() {
       <p className='car__info'>
         {carObject.description}
       </p>
-
       <span className={carObject.inStock ? "car__stock" : "car__not-in-stock"}>
         {carObject.inStock ? "In stock" : "Not in stock"}
       </span>
+
+      <h4 className="car__title2">
+        Available colors
+      </h4>
+      <div className="car__colors">
+        <div className="car__color">
+          <span className="car__color-name">
+            Tomato Red
+          </span>
+          <span 
+            className="car__color-name-box" 
+            style={{
+              backgroundColor: '#C34A2C'
+            }}
+          ></span>
+        </div>
+      </div>
     </div>
   );
 }
