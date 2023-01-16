c13910e94b66b756543944c6bebdb5432793906c
Serzsh <serzants.janis@gmail.com>
2023-01-16 14:45:39 +0200

    updates

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -3,6 +3,7 @@ import './counter.css'
 
 
 const Counter = () => {
+    const [name, setName] = useState("123")
     const [count, setCount] = useState(0)
     const [person, setPerson] = useState({
         name: 'Jānis',
@@ -10,8 +11,35 @@ const Counter = () => {
         likeCats: true
     })
 
+    const someObj = {
+        name: 'John',
+        lastName: 'Doe'
+    }
+
+    const someOtherObject = {
+        ...someObj,
+        lastName: 'Bērziņš',
+        age: 50
+    }
+
+    console.log(someOtherObject);
+
+    // events inputam
+    // state
+    // parādīt state
+
     return (
         <div className='counter'>
+            <h2>My name is: {name}</h2>
+            <input 
+                type="text" 
+                placeholder='Type your name'
+                value={name}
+                onChange={() => {
+                    // change state to current value
+                }}
+            />
+            <br /><br /><br />
             <h1>{count}</h1>
             <button 
                 onClick={() => {
@@ -36,31 +64,28 @@ const Counter = () => {
             <h1>Person:</h1>
             <h2>Person name is: {person.name}</h2>
             <h2>Person age is: {person.age}</h2>
+            <h2>Person likes cats: {person.likeCats ? 'yes' : 'no'}</h2>
             <button 
-            onClick={() => {
-                setPerson({
-                    name: 'John',
-                    age: person.age,
-                    likeCats: person.likeCats,
-                })
-            }}
+                onClick={() => {
+                    setPerson({
+                    ...person,
+                        name: 'John',
+                    })
+                }}
             >
                 Change name to John
             </button>
             <button onClick={() => {
                 setPerson({
-                    likeCats: person.likeCats,
-                    name: person.name,
+                    ...person,
                     age: person.age + 1
                 })
             }}>
                 Add +1 to age
             </button>
+            <br /><br /><br />
         </div>
     )
 }
 
 export default Counter
-
-// events - click, mouseover, sumbit
-// state - 
